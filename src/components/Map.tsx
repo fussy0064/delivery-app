b5b3b62b4908391104d62dd7984eefab414bd682
SwiftDeliver <delivery@example.com>
2026-09-07 19:55:50 +0000

    Live moving driver on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-// Fix default marker icons in Next.js
-const icon = L.icon({
+const driverIcon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
@@ -14,23 +13,60 @@ const icon = L.icon({
   iconAnchor: [12, 41],
 });
 
+const destIcon = L.icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
 type Props = {
-  driverLat?: number;
-  driverLng?: number;
+  startLat?: number;
+  startLng?: number;
   destLat?: number;
   destLng?: number;
 };
 
-export default function Map({ driverLat = -6.7924, driverLng = 39.2083, destLat = -6.7800, destLng = 39.2200 }: Props) {
+// Small helper to keep map centered on driver
+function Recenter({ lat, lng }: { lat: number; lng: number }) {
+  const map = useMap();
   useEffect(() => {
-    // Fix for default markers
-    // @ts-ignore
-    delete L.Icon.Default.prototype._getIconUrl;
+    map.setView([lat, lng], map.getZoom());
+  }, [lat, lng, map]);
+  return null;
+}
+
+export default function Map({
+  startLat = -6.7924,
+  startLng = 39.2083,
+  destLat = -6.78,
+  destLng = 39.22,
+}: Props) {
+  const [driverPos, setDriverPos] = useState<[number, number]>([startLat, startLng]);
+  const [progress, setProgress] = useState(0);
+
+  // Live movement simulation
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) return 100;
+        return prev + 0.8; // speed of movement
+      });
+    }, 400);
+
+    return () => clearInterval(interval);
   }, []);
+
+  // Calculate current position between start and destination
+  useEffect(() => {
+    const lat = startLat + (destLat - startLat) * (progress / 100);
+    const lng = startLng + (destLng - startLng) * (progress / 100);
+    setDriverPos([lat, lng]);
+  }, [progress, startLat, startLng, destLat, destLng]);
 
   return (
     <MapContainer
-      center={[driverLat, driverLng]}
+      center={driverPos}
       zoom={14}
       style={{ height: "100%", width: "100%", borderRadius: "1rem" }}
       scrollWheelZoom={false}
@@ -39,12 +75,32 @@ export default function Map({ driverLat = -6.7924, driverLng = 39.2083, destLat 
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[driverLat, driverLng]} icon={icon}>
-        <Popup>Driver is here</Popup>
+
+      {/* Route line */}
+      <Polyline
+        positions={[
+          [startLat, startLng],
+          [destLat, destLng],
+        ]}
+        color="#f97316"
+        weight={4}
+        opacity={0.7}
+      />
+
+      {/* Moving driver */}
+      <Marker position={driverPos} icon={driverIcon}>
+        <Popup>
+          Driver is moving<br />
+          Progress: {Math.round(progress)}%
+        </Popup>
       </Marker>
-      <Marker position={[destLat, destLng]} icon={icon}>
+
+      {/* Destination */}
+      <Marker position={[destLat, destLng]} icon={destIcon}>
         <Popup>Delivery location</Popup>
       </Marker>
+
+      <Recenter lat={driverPos[0]} lng={driverPos[1]} />
     </MapContainer>
   );
 }
